1f36d67fe363e8e28bf0e6725b892044a478f7f3
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-04 16:50:56 +0200

    Fixed voice pages

--- plugins/juci-asterisk/src/pages/status.phone.js
+++ plugins/juci-asterisk/src/pages/status.phone.js
@@ -1,7 +1,7 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
 
 JUCI.app
-.controller("StatusVoicePageCtrl", function($scope, $rpc, $uci){
+.controller("StatusPhone", function($scope, $rpc, $uci){
 	if($rpc.asterisk){
 		$scope.voice_available = true; 
 		JUCI.interval.repeat("voice.status", 4000, function(done){
