97e951a2d4ea40f7280217e1711c73a0b3bedfb7
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-04 18:37:58 +0200

    Added DECT support in gui

--- plugins/juci-natalie-dect/src/pages/phone.dect.js
+++ plugins/juci-natalie-dect/src/pages/phone.dect.js
@@ -1,5 +1,23 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
 JUCI.app
 .controller("PhoneDectPage", function($scope, $rpc, gettext){
-	
+	if($rpc.sys && $rpc.sys.dect){
+		JUCI.interval.repeat("dect.refresh", 1000, function(done){
+			$rpc.sys.dect.status().done(function(stats){
+				$scope.status = stats; 
+				$scope.$apply(); 
+				done(); 
+			}); 
+		}); 
+		$scope.onStartPairing = function(){
+			$rpc.sys.dect.pair().done(function(){
+				
+			}); 
+		}
+		$scope.onPingHandset = function(hs){
+			$rpc.sys.dect.ping({ id: hs.id }).done(function(){
+				
+			}); 
+		}
+	}
 }); 
