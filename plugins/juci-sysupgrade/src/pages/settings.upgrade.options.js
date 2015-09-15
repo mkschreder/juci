020775444c33668b7852e97182e611b99d9c667e
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-15 08:52:05 +0200

    sysupgrade: fixed .y extension for usb upgrade

--- plugins/juci-sysupgrade/src/pages/settings.upgrade.options.js
+++ plugins/juci-sysupgrade/src/pages/settings.upgrade.options.js
@@ -4,7 +4,7 @@ JUCI.app
 .controller("SettingsUpgradeOptions", function($scope, $config, $uci, $rpc, $tr, gettext){
 	$scope.allImageExtensions = [
 		{ label: $tr(gettext(".w (JFFS Image)")), value: ".w" },
-		{ label: $tr(gettext(".y (UBIFS Image)")), value: ".w" }, 
+		{ label: $tr(gettext(".y (UBIFS Image)")), value: ".y" }, 
 		{ label: $tr(gettext("fs_image (RAW Rootfs Image)")), value: "fs_image" }
 	]; 
 	
