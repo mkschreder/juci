4304f2a9ac1d8d4d837341cf657c5d46be8f991a
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-16 12:11:57 +0200

    juci-network: set type to none in view when type is emtpty to get correct edit widget displayed

--- plugins/juci-network-netifd/src/widgets/network-connection-edit.js
+++ plugins/juci-network-netifd/src/widgets/network-connection-edit.js
@@ -53,7 +53,7 @@ JUCI.app
 	
 	$scope.$watch("conn", function(iface){
 		if(!iface) return; 
-		iface.$type_editor = "<network-connection-type-"+iface.type.value+"-edit ng-model='conn'/>"; 
+		iface.$type_editor = "<network-connection-type-"+(iface.type.value||'none')+"-edit ng-model='conn'/>"; 
 		iface.$proto_editor = "<network-connection-proto-"+iface.proto.value+"-edit ng-model='conn'/>"; 
 		$rpc.network.interface.dump().done(function(ifaces){
 			var info = ifaces.interface.find(function(x){ return x.interface == iface[".name"]; }); 
