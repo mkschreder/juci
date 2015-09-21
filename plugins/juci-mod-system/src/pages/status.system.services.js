56b42e9890135c3221f3e226168612498e2b99d5
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-21 15:25:50 +0200

    juci-mod-system: added new services lua backend stub

--- plugins/juci-mod-system/src/pages/status.system.services.js
+++ plugins/juci-mod-system/src/pages/status.system.services.js
@@ -1,27 +1,21 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
+
 JUCI.app
 .controller("ServicesStatusPage", function($scope, $rpc, gettext){
-	$rpc.service.list().done(function(result){
-		var services = []; 
-		Object.keys(result).map(function(k){
-			var service = result[k]; 
-			if(service.instances){
-				var instance = 0; 
-				Object.keys(service.instances).map(function(ik){
-					var inst = service.instances[ik]; 
-					if(inst.command)
-						inst.command = inst.command.join(" "); 
-					inst.name = k+"."+instance; 
-					instance++; 
-					services.push(inst); 
-				}); 
-			} else {
-				service.name = k; 
-				service.running = false; 
-				services.push(service); 
-			}
-		}); 
-		$scope.services = services; 
+	$rpc.juci.system.service.list().done(function(result){
+		$scope.services = result.services; 
 		$scope.$apply(); 
 	}); 
+
+	$scope.onServiceEnable = function(service){
+	
+	}
+	
+	$scope.onServiceReload = function(service){
+	
+	}
+
+	$scope.onServiceToggle = function(service){
+
+	}
 }); 
