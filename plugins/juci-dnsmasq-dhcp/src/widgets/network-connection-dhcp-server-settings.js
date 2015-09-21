3465c81e8dc3c6a377954359138b4b2da6a73d5b
Martin Schröder <mkschreder.uk@gmail.com>
2015-09-21 11:31:29 +0200

    juci-dnsmasq-dhcp: create dhcp section if it does not exist

--- plugins/juci-dnsmasq-dhcp/src/widgets/network-connection-dhcp-server-settings.js
+++ plugins/juci-dnsmasq-dhcp/src/widgets/network-connection-dhcp-server-settings.js
@@ -19,14 +19,27 @@ JUCI.app
 		if(!value) return; 
 		$uci.sync("dhcp").done(function(){
 			$scope.dhcp = $uci.dhcp["@dhcp"].find(function(x){
-				return x.interface.value == value[".name"]; 
+				return x.interface.value == value[".name"] || x[".name"] == value[".name"]; 
 			}); 
 			$scope.data.dhcpEnabled = $scope.dhcp && !$scope.dhcp.ignore.value; 
 			$scope.$apply(); 
 		}); 
 	}); 
 	$scope.$watch("data.dhcpEnabled", function(value){
-		if(!$scope.dhcp) return; 
-		$scope.dhcp.ignore.value = value; 
+		if(!$scope.dhcp) {
+			if($scope.connection){
+				$uci.dhcp.create({
+					".type": "dhcp", 
+					".name": $scope.connection[".name"],
+					"interface": $scope.connection[".name"],
+					"ignore": !value
+				}).done(function(dhcp){
+					$scope.dhcp = dhcp; 
+					$scope.$apply(); 
+				}); 
+			}
+		} else {
+			$scope.dhcp.ignore.value = !value; 
+		}
 	}); 
 }); 
