f104f4d6e035b29e24b996e6c215930a25116a7c
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-02 11:15:13 +0200

    juci-upnp: add upnp under network services submenu

--- plugins/juci-upnp/src/pages/internet.services.upnp.js
+++ plugins/juci-upnp/src/pages/internet.services.upnp.js
@@ -1,1 +1,9 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
+
+JUCI.app
+.controller("UPNPMainPage", function($scope, $uci, $network, $config, $firewall, $upnp, $tr, gettext){
+	$upnp.getConfig().done(function(config){
+		$scope.upnp = config;
+		$scope.$apply();  
+	}); 
+}); 
