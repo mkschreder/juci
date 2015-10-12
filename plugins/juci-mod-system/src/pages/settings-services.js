bd037baf908771a1451e8c81cca69927c6d37639
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-12 17:59:48 +0200

    juci-mod-system: add $systemService factory for listing and starting/stopping system services with ease

--- plugins/juci-mod-system/src/pages/settings-services.js
+++ plugins/juci-mod-system/src/pages/settings-services.js
@@ -5,12 +5,12 @@ JUCI.app
 	JUCI.interval.repeat("juci-services-page", 5000, function(done){
 		$rpc.juci.system.service.list().done(function(result){
 			$scope.services = result.services; 
-			for (i=0; i < $scope.services.length;i++){
-				$scope.services[i].reload = false;
-				if(!$scope.services[i].start_priority){
-					$scope.services[i].start_priority = 9999;
+			$scope.services.map(function(service){
+				service.reload = false;
+				if(!service.start_priority){
+					service.start_priority = 9999;
 				}
-			}
+			}); 
 			$scope.services.sort(function(a,b){
 				return a.start_priority - b.start_priority;
 			});
@@ -20,12 +20,12 @@ JUCI.app
 	}); 
 
 	$scope.onServiceEnable = function(service){
-		if(!service.enabled){
+		if(service.enabled){
 			$rpc.juci.system.service.disable(service).done(function(result){
 				console.log("service: " + service.name + " is disabled");
 				$scope.$apply(); 
 			});	
-		}else{
+		} else {
 			$rpc.juci.system.service.enable(service).done(function(result){
 				console.log("service: " + service.name + " is enabled");
 				$scope.$apply();
@@ -45,13 +45,13 @@ JUCI.app
 	$scope.onServiceToggle = function(service){
 		if(service.running){
 			$rpc.juci.system.service.stop(service).done(function(result){
-				service.running = false;
+				service.running = true;
 				console.log("service: " + service.name + " is stoped");
 				$scope.$apply(); 
 			});	
-		}else{
+		} else {
 			$rpc.juci.system.service.start(service).done(function(result){
-				service.running = true;
+				service.running = false;
 				console.log("service: " + service.name + " is started");
 				$scope.$apply();
 			});
--- plugins/juci-mod-system/src/system.js
+++ plugins/juci-mod-system/src/system.js
@@ -24,3 +24,58 @@ UCI.system.$registerSectionType("upgrade", {
 	"fw_find_ext":		{ dvalue: "", type: String, required: false},
 	"fw_upload_path":	{ dvalue: "", type: String, required: false}
 }); 
+
+JUCI.app.factory("$systemService", function($rpc){
+	return {
+		list: function(){
+			var def = $.Deferred(); 
+			var self = this; 
+			$rpc.juci.system.service.list().done(function(result){
+				if(result && result.services){
+					var result = result.services.map(function(service){
+						service.enable = function(){
+							var self = this; 
+							console.log("enabling service "+self.name); 
+							return $rpc.juci.system.service.enable({ name: self.name }).done(function(){ self.enabled = true; }); 
+						}
+						service.disable = function(){
+							var self = this; 
+							console.log("disabling service "+self.name); 
+							return $rpc.juci.system.service.disable({ name: self.name }).done(function(){ self.enabled = false; });
+						}
+						service.start = function(){
+							var self = this; 
+							console.log("starting service "+self.name); 
+							return $rpc.juci.system.service.start({ name: self.name }).done(function(){ self.running = true; }); 
+						}
+						service.stop = function(){
+							var self = this; 
+							console.log("stopping service "+self.name); 
+							return $rpc.juci.system.service.stop({ name: self.name }).done(function(){ self.running = false; }); 
+						}
+						service.reload = function(){
+							var self = this; 
+							return $rpc.juci.system.service.reload({ name: self.name }); 
+						}
+						return service;	
+					}); 
+					def.resolve(result); 
+				} else {
+					def.reject(); 
+				}
+			}).fail(function(){ def.reject(); }); 
+			return def.promise(); 
+		},
+		find: function(name){
+			var def = $.Deferred(); 
+			this.list().done(function(services){
+				if(services) {
+					def.resolve(services.find(function(x){ return x.name == name; })); 
+				} else {
+					def.reject(); 
+				}
+			}).fail(function(){ def.reject(); }); 
+			return def.promise(); 
+		}
+	}; 
+}); 
