f1d9ab8def104464716ed52d3da132521f33247d
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-14 17:18:11 +0200

    juci-ddns: implement proper ddns settings editing

--- plugins/juci-ddns/src/pages/internet-services-ddns.js
+++ plugins/juci-ddns/src/pages/internet-services-ddns.js
@@ -1,36 +1,29 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
 JUCI.app
 .controller("DDNSPage", function ($scope, $uci, $network, $config) {
-	
 	$scope.data = {}; 
 	$uci.$sync(["ddns"]).done(function () {
-		$network.getWanNetworks().done(function(nets){
-			// get the wan interface and fetch the ddns config for it. Currently we manually specify a single "internet" interface. 
-			$scope.wan_network = nets.find(function(x){ return x[".name"] == $config.wan_interface; }); 
-			if($scope.wan_network) {
-				$scope.wan_network.$ddns = $uci.ddns["@service"].find(function(x){ return x.interface.value == $scope.wan_network[".name"]; }); 
-			}
+		$scope.ddns_list = $uci.ddns["@service"]; 
+		$scope.$apply(); 
+	}); 
+
+	$scope.onAddDdnsSection = function(){
+		$uci.ddns.$create({
+			".type": "service", 
+			".name": "new ddns config",
+			"enabled": true
+		}).done(function(ddns){
 			$scope.$apply(); 
 		}); 
-	}); 
+	} 
 	
-	$scope.onToggleDDNS = function(){
-		console.log("toggle: "+$scope.data.ddnsEnabled); 
-		if(!$scope.wan_network) return; 
-		if(!$scope.wan_network.$ddns){
-			$uci.ddns.create({
-				".type": "service", 
-				"interface": $scope.wan_network[".name"]
-			}).done(function(ddns){
-				$scope.wan_network.$ddns = ddns; 
-				ddns.enabled.value = $scope.data.ddnsEnabled; 
-				$scope.$apply(); 
-			}); 
-		} else {
-			$scope.wan_network.$ddns.enabled.value = $scope.data.ddnsEnabled; 
-		}
+	$scope.onRemoveDdnsSection = function(ddns){
+		if(!ddns) return; 
+		ddns.$delete().done(function(){
+			$scope.$apply(); 
+		});  
 	}
-	
+
 	$scope.getItemTitle = function(item){
 		return item[".name"]; 
 	}
--- plugins/juci-ddns/src/widgets/ddns-network-settings-edit.js
+++ plugins/juci-ddns/src/widgets/ddns-network-settings-edit.js
@@ -4,19 +4,34 @@ JUCI.app
 .directive("ddnsNetworkSettingsEdit", function($compile){
 	return {
 		scope: {
-			network: "=ngModel"
+			ddns: "=ngModel"
 		}, 
 		templateUrl: "/widgets/ddns-network-settings-edit.html", 
 		controller: "ddnsNetworkSettingsEdit"
 	};
 })
-.controller("ddnsNetworkSettingsEdit", function($scope){
-	// hardcoded dns providers that we currently support (TODO: make dynamic list)
-	$scope.allServices = [
-		{ label: "noip.com", value: "noip.com" }
+.controller("ddnsNetworkSettingsEdit", function($scope, $rpc, $tr, gettext, $ethernet, $network){
+	$scope.allSourceTypes = [
+		{ label: $tr(gettext("Interface")), value: "interface" }, 
+		{ label: $tr(gettext("Network")), value: "network" }, 
+		{ label: $tr(gettext("Script")), value: "script" }, 
+		{ label: $tr(gettext("Web")), value: "web" }
 	]; 
-	$scope.$watch("network", function(value){
-		if(!value) return; 
-		$scope.ddns = value; 
+	$ethernet.getAdapters().done(function(adapters){
+		$scope.allSourceDevices = adapters.map(function(a){
+			return { label: a.name, value: a.device }; 
+		}); 
+		$scope.$apply(); 
+	});
+	$network.getNetworks().done(function(nets){
+		$scope.allSourceNetworks = nets.map(function(n){
+			return { label: n[".name"], value: n[".name"] }; 
+		}); 
+		$scope.$apply(); 
+	}); 
+	$rpc.juci.ddns.providers().done(function(result){
+		if(!result || !result.providers) return; 
+		$scope.allServices = result.providers.map(function(p){ return { label: p, value: p }}); 
+		$scope.$apply(); 
 	}); 
 }); 
