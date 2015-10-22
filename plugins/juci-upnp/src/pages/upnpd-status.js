a0ac5feacaf080f41ab61c47f2d6cdfaa523ecd2
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-22 16:24:26 +0200

    juci-upnp: add upnp status page that shows upnp open ports

---
+++ plugins/juci-upnp/src/pages/upnpd-status.js
@@ -0,0 +1,9 @@
+//! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
+
+JUCI.app
+.controller("UPNPStatusPage", function($scope, $upnp){	
+	$rpc.juci.upnpd.ports().done(function(result){
+		$scope.upnpOpenPorts = result.ports; 
+		$scope.$apply(); 
+	}); 
+}); 
