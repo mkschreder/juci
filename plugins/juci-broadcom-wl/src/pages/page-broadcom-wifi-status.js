0ab84e0116054a795efb487f2d1b915b9ba7a74b
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-23 11:34:00 +0200

    juci-broadcom-wl: allow scanning all radios and warn when user has dfs enabled (can not scan on that radio then so not all bands will be scanned)

--- plugins/juci-broadcom-wl/src/pages/page-broadcom-wifi-status.js
+++ plugins/juci-broadcom-wl/src/pages/page-broadcom-wifi-status.js
@@ -6,15 +6,24 @@ JUCI.app
 		$scope.predicate = pred; 
 		$scope.reverse = ($scope.predicate === pred) ? !$scope.reverse : false;
 	}
-
-	JUCI.interval.repeat("wifi-status", 8000, function(done){
-		$wireless.scan(); 
-		setTimeout(function(){
-			$wireless.getScanResults().done(function(aps){
-				$scope.access_points = aps;
-				$scope.$apply(); 
-				done(); 
-			});
-		}, 4000); 
+	$uci.$sync("wireless").done(function(){
+		$scope.dfs_enabled = $uci.wireless["@wifi-device"].find(function(x){ return x.dfsc.value != 0; }) != null; 
+		$scope.doScan = function(){
+			$scope.scanning = 1; 
+			async.eachSeries($uci.wireless["@wifi-device"].filter(function(x){ return x.dfsc.value != 0; }), function(dev, next){
+				console.log("Scanning on "+dev[".name"]); 
+				$wireless.scan({device: dev[".name"]}).done(function(){
+					setTimeout(function(){
+						console.log("Getting scan results for "+dev[".name"]); 
+						$wireless.getScanResults({device: dev[".name"]}).done(function(aps){
+							$scope.access_points = aps;
+							$scope.scanning = 0; 
+							$scope.$apply(); 
+						}); 
+					}, 4000); 
+					next(); 
+				}); 
+			}); 
+		} 
 	}); 
 }); 
