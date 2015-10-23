c389f90d837422cea65786576e24f58432a7bc31
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-10-23 16:03:51 +0200

    juci-minidlna: fixed filetree add and added list method to minidlna ubus object

--- plugins/juci-minidlna/src/pages/internet-services-minidlna.js
+++ plugins/juci-minidlna/src/pages/internet-services-minidlna.js
@@ -5,6 +5,21 @@ JUCI.app
 	$scope.data = [{label:"loading"}];
 	$minidlna.getConfig().done(function(config){
 		$scope.config = config; 
+		$scope.tagslistData = $scope.config.media_dir.value.filter(function(dir){
+			return (dir.substring(0, 4) == "/mnt");
+		}).map(function(dir){
+			if(dir == "/mnt"){
+				return {
+					text: "/",
+					path: "/mnt/"
+				}
+			}
+			return {
+				text: dir.substring(4),
+				path: dir
+			}
+		
+		});
 		$scope.$apply();
 		$scope.onAddFolder = function(){
 			var model = {}
@@ -13,17 +28,29 @@ JUCI.app
 				model: model,
 				on_apply: function(btn, dlg){
 					if(!model.selected || !model.selected.path)return false;
-					$scope.config.media_dir.value.push(model.selected.path);
-					console.log(JSON.stringify($scope.config.media_dir.value));
+					for(var i=0; i < $scope.tagslistData.length; i++){
+						if($scope.tagslistData[i].path == model.selected.path) return false;
+					}
+					$scope.tagslistData.push({
+						path: model.selected.path,
+						text: model.selected.path.substring(4)
+					});
+					$scope.updateConfig();
 					return true;
 				}	
 			});
 		};
-		$scope.onDeleteFolder = function(item){
-			var index = $scope.config.media_dir.value.indexOf(item);
-			if(index > -1){
-				$scope.config.media_dir.value.splice(index, 1);
-			}
+		$scope.updateConfig =  function(){
+			$scope.config.media_dir.value = $scope.tagslistData.map(function(dir){
+				return dir.path;
+			});
+		};
+		var tag_promise = null;
+		$scope.loadTags = function(text){
+			if(!tag_promise)tag_promise = $rpc.juci.minidlna.list({path:text}).allways(function(){
+				tag_promise = null;
+			});
+			return tag_promise;
 		};
 	}); 
 }); 
