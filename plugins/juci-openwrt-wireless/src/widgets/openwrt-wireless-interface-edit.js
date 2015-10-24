0b8d1e46ac0766779a0b3df9d94cee3ca73fddcf
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-24 19:55:20 +0200

    juci-openwrt-wireless: fixup encryption options to match openwrt documentation

--- plugins/juci-openwrt-wireless/src/widgets/openwrt-wireless-interface-edit.js
+++ plugins/juci-openwrt-wireless/src/widgets/openwrt-wireless-interface-edit.js
@@ -18,47 +18,49 @@ JUCI.app
 		ev.stopPropagation(); 
 		$scope.errors.push(err); 
 	}); 
+	$scope.cryptoChoices = [
+		{ label: $tr(gettext("None")), value: "none" }, 
+		{ label: $tr(gettext("WEP")), value: "wep" }, 
+		{ label: $tr(gettext("WPA2 Personal (PSK)")), value: "psk2" }, 
+		{ label: $tr(gettext("WPA Personal (PSK)")), value: "psk" }, 
+		{ label: $tr(gettext("WPA/WPA2 Personal (PSK) Mixed Mode")), value: "psk-mixed" }, 
+		{ label: $tr(gettext("WPA2 Enterprise")), value: "wpa2" }, 
+		{ label: $tr(gettext("WPA Enterprise")), value: "wpa" }, 
+		{ label: $tr(gettext("WPA/WPA2 Enterprise Mixed Mode")), value: "wpa-mixed" } 
+	]; 
+	
 	$scope.keyChoices = [
 		{label: $tr(gettext("Key")) + " #1", value: 1},
 		{label: $tr(gettext("Key")) + " #2", value: 2},
 		{label: $tr(gettext("Key")) + " #3", value: 3},
 		{label: $tr(gettext("Key")) + " #4", value: 4}
 	];
+
 	$scope.psk2_ciphers = [
 		{label: $tr(gettext("Auto")), value: "auto"},
 		{label: $tr(gettext("CCMP (AES)")), value: "ccmp"}
 	]; 
+
 	$scope.mixed_psk_ciphers = [
 		{label: $tr(gettext("Auto")), value: "auto"},
 		{label: $tr(gettext("CCMP (AES)")), value: "ccmp"},
 		{label: $tr(gettext("TKIP/CCMP (AES)")), value: "ccmp"}
 	];  
+
 	$network.getNetworks().done(function(nets){
 		$scope.networks = nets.map(function(net){
 			return { label: String(net[".name"]).toUpperCase(), value: net[".name"] }; 
 		}); 
 		$scope.$apply(); 
 	}); 
+
 	$wireless.getDevices().done(function(devices){
 		$scope.devices = devices.map(function(x){
 			return { label: x[".frequency"], value: x[".name"] }; 
 		}); 
 		$scope.$apply(); 
 	}); 
-	$scope.$watch("iface", function(value){
-		if(!value) return; 
-		try {
-			$scope.cryptoChoices = [
-				{ label: $tr(gettext("None")), value: "none" }, 
-				{ label: $tr(gettext("WEP")), value: "wep" }, 
-				{ label: $tr(gettext("WPA-PSK")), value: "psk" }, 
-				{ label: $tr(gettext("WPA/WPA2 Personal (PSK) Mixed")), value: "psk-mixed" }, 
-				{ label: $tr(gettext("WPA2-PSK")), value: "psk2" }, 
-				{ label: $tr(gettext("WPA2 Enterprise")), value: "wpa2" }, 
-				{ label: $tr(gettext("WPA-MIXED")), value: "wpa+mixed" }, 
-			]; 
-		} catch(e) {} 
-	});
+
 	$scope.$watch("iface.closed.value", function(value, oldvalue){
 		if(!$scope.iface) return; 
 		if(value && value != oldvalue){
