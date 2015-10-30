887dde65bba5d3c8b95ae3e8bd4bf13d09c66451
Martin Schröder <mkschreder.uk@gmail.com>
2015-10-31 00:45:22 +0100

    juci-snmp: fix missing $uci parameter

--- plugins/juci-snmp/src/pages/snmp-config.js
+++ plugins/juci-snmp/src/pages/snmp-config.js
@@ -1,7 +1,7 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
 
 JUCI.app
-.controller("SnmpConfigPage", function($scope, gettext){
+.controller("SnmpConfigPage", function($scope, $uci, gettext){
 	$uci.$sync("snmpd").done(function(){
 		if(!$uci.snmpd || !$uci.snmpd["@mini_snmpd"].length) return; 
 		$scope.config = $uci.snmpd["@mini_snmpd"]; 
