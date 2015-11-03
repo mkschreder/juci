4ba54cc82dee8efeb14a3ca60b2e38ba218a4e8f
Martin Schröder <mkschreder.uk@gmail.com>
2015-11-03 12:51:23 +0100

    juci-firewall-fw3: move maclist to firewall plugin

--- plugins/juci-firewall-fw3/src/widgets/firewall-maclist-edit.js
+++ plugins/juci-firewall-fw3/src/widgets/firewall-maclist-edit.js
@@ -1,22 +1,21 @@
 //! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
 
 JUCI.app
-.directive("juciMaclistEdit", function($compile){
+.directive("firewallMaclistEdit", function($compile){
 	return {
-		templateUrl: "/widgets/juci-maclist-edit.html", 
+		templateUrl: "/widgets/firewall-maclist-edit.html", 
 		scope: {
 			macList: "=ngModel"
 		}, 
-		controller: "juciMaclistEdit", 
+		controller: "firewallMaclistEdit", 
 		replace: true
 	 };  
 })
-.controller("juciMaclistEdit", function($scope, $config, $uci, $rpc, $network, $localStorage, $state, gettext){ 
-	
+.controller("firewallMaclistEdit", function($scope, $config, $uci, $rpc, $network, $localStorage, $state, gettext){ 
 	$network.getConnectedClients().done(function(clients){
 		$scope.connectedHosts = clients.map(function(client){
 			return { 
-				label: client.hostname+" ("+client.ipaddr+")", 
+				label: (client.hostname)?(client.hostname +" ("+client.ipaddr+")"):client.ipaddr, 
 				value: client.macaddr 
 			}; 
 		}); 
