33d0fe84d9c48b7365fc8e89869e6fec38c55dc0
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-11-06 16:58:58 +0100

    juci-broadcom-ethernet: added switch to set wan in bridge mode

--- plugins/juci-broadcom-ethernet/src/pages/page-broadcom-ethernet-phy.js
+++ plugins/juci-broadcom-ethernet/src/pages/page-broadcom-ethernet-phy.js
@@ -10,6 +10,7 @@ JUCI.app
 	$uci.$sync(["ports", "layer2_interface_ethernet"]).done(function(){
 		$scope.ports = $uci.ports["@ethport"];
 		$scope.data.wan_port = $scope.ports.find(function(x){ return x.ifname.value == $uci.layer2_interface_ethernet.Wan.baseifname.value; }); 
+		$scope.config = $uci.layer2_interface_ethernet.Wan; 
 		
 		$scope.$watch("data.wan_port", function(value){
 			if(!value) return; 
