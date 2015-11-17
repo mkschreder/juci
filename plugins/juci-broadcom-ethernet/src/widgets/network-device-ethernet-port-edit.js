a31b36f51bdb3931ffafcdc21e42c4dc16d44766
Martin Schröder <mkschreder.uk@gmail.com>
2015-11-17 11:46:50 +0100

    juci-broadcom-ethernet: remove unnecessary code

--- plugins/juci-broadcom-ethernet/src/widgets/network-device-ethernet-port-edit.js
+++ plugins/juci-broadcom-ethernet/src/widgets/network-device-ethernet-port-edit.js
@@ -11,25 +11,17 @@ JUCI.app
 		replace: true
 	};  
 })
-.controller("networkDeviceEthernetPortEdit", function($scope, $network, gettext, $tr){
-	$network.getDevices().done(function(devices){
-		
-		$scope.speeds = [
-			{ label: $tr(gettext("Full auto-negotiation")), 					value: "auto" }, 
-			{ label: $tr(gettext("Max 100Mb auto-negotiation, full duplex")), 	value: "100FDAUTO" },
-			{ label: $tr(gettext("Max 100Mb auto-negotiation, half duplex")), 	value: "100HDAUTO" },
-			{ label: $tr(gettext("Max 10Mb auto-negotiation, full duplex")), 	value: "10FDAUTO" },
-			{ label: $tr(gettext("Max 10Mb auto-negotiation, half duplex")), 	value: "10HDAUTO" },
-			{ label: $tr(gettext("Only 100Mb, full duplex")), 					value: "100FD" },
-			{ label: $tr(gettext("Only 100Mb, half duplex")), 					value: "100HD" },
-			{ label: $tr(gettext("Only 10Mb, full duplex")), 					value: "10FD" },
-			{ label: $tr(gettext("Only 10Mb, half duplex")), 					value: "10HD" }, 
-			{ label: $tr(gettext("Disabled")), 									value: "disabled" }
-		]; 
-		
-
-		$scope.$apply(); 
-		
-	}); 
-	
+.controller("networkDeviceEthernetPortEdit", function($scope, gettext, $tr){
+	$scope.speeds = [
+		{ label: $tr(gettext("Full auto-negotiation")), 					value: "auto" }, 
+		{ label: $tr(gettext("Max 100Mb auto-negotiation, full duplex")), 	value: "100FDAUTO" },
+		{ label: $tr(gettext("Max 100Mb auto-negotiation, half duplex")), 	value: "100HDAUTO" },
+		{ label: $tr(gettext("Max 10Mb auto-negotiation, full duplex")), 	value: "10FDAUTO" },
+		{ label: $tr(gettext("Max 10Mb auto-negotiation, half duplex")), 	value: "10HDAUTO" },
+		{ label: $tr(gettext("Only 100Mb, full duplex")), 					value: "100FD" },
+		{ label: $tr(gettext("Only 100Mb, half duplex")), 					value: "100HD" },
+		{ label: $tr(gettext("Only 10Mb, full duplex")), 					value: "10FD" },
+		{ label: $tr(gettext("Only 10Mb, half duplex")), 					value: "10HD" }, 
+		{ label: $tr(gettext("Disabled")), 									value: "disabled" }
+	]; 
 }); 
