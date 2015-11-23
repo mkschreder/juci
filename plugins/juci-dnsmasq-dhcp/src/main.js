1b95edadbdcb6a35a14117b4349b1a723285c27d
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-11-23 17:21:56 +0100

    juci-dnsmasq-dhcp: done with settings page

--- plugins/juci-dnsmasq-dhcp/src/main.js
+++ plugins/juci-dnsmasq-dhcp/src/main.js
@@ -3,16 +3,31 @@
 UCI.$registerConfig("dhcp"); 
 UCI.dhcp.$registerSectionType("dnsmasq", {
 	"domainneeded":		{ dvalue: true, type: Boolean },
+	"dhcpleasemax":		{ dvalue: undefined, type: Number },
 	"boguspriv":		{ dvalue: true, type: Boolean },
 	"localise_queries":	{ dvalue: true, type: Boolean },
-	"rebind_protection":	{ dvalue: false, type: Boolean },
-	"local":		{ dvalue: "", type: String, required: true},
-	"domain":		{ dvalue: "", type: String, required: true},
+	"rebind_protection":{ dvalue: false, type: Boolean },
+	"rebind_localhost":	{ dvalue: false, type: Boolean },
+	"dnsforwardmax":	{ dvalue: undefined, type: Number },
+	"rebind_domain":	{ dvalue: [], type: Array },
+	"ednspacket_max":	{ dvalue: undefined, type: Number },
+	"local":			{ dvalue: "", type: String, required: true},
+	"port":				{ dvalue: 53, type: Number },
+	"domain":			{ dvalue: "", type: String, required: true},
+	"logqueries":		{ dvalue: false, type: Boolean },
+	"filterwin2k":		{ dvalue: false, type: Boolean },
+	"queryport":		{ dvalue: undefined, type: Number },
+	"addnhosts":		{ dvalue: [], type: Array },
+	"bogusnxdomain":	{ dvalue: [], type: Array },
+	"server":			{ dvalue: [], type: Array },
+	"noresolv":			{ dvalue: false, type: Boolean },
+	"nonegcache":		{ dvalue: false, type: Boolean },
+	"strictorder":		{ dvalue: false, type: Boolean },
 	"expandhosts":		{ dvalue: true, type: Boolean },
 	"authoritative":	{ dvalue: true, type: Boolean },
 	"readethers":		{ dvalue: true, type: Boolean },
-	"leasefile":		{ dvalue: "/tmp/dhcp.leases", type: String, required: true},
-	"resolvfile":		{ dvalue: "/tmp/resolv.conf.auto", type: String, required: true}
+	"leasefile":		{ dvalue: "/tmp/dhcp.leases", type: String },
+	"resolvfile":		{ dvalue: "/tmp/resolv.conf.auto", type: String }
 });
 UCI.dhcp.$registerSectionType("dhcp", {
 	"interface":		{ dvalue: "", type: String, required: true},
---
+++ plugins/juci-dnsmasq-dhcp/src/pages/dns-dhcp-settings.js
@@ -0,0 +1,36 @@
+//! Author: Reidar Cederqvist <reidar.cederqvist@gmail.com>
+
+JUCI.app
+.controller("dnsDhcpSettingsCtrl", function($scope, $uci){
+	$uci.$sync(["dhcp"]).done(function(){
+		$scope.dnsmasq = $uci.dhcp["@dnsmasq"][0];
+		$scope.hostfiles = $scope.dnsmasq.addnhosts.value.map(function(x){
+			return { label: x };
+		});
+		$scope.bogusnxdomain = $scope.dnsmasq.bogusnxdomain.value.map(function(host){ return { label: host }});
+		$scope.server = $scope.dnsmasq.server.value.map(function(server){ return { label: server }});
+		$scope.rebind_domain = $scope.dnsmasq.rebind_domain.value.map(function(domain){ return { label: domain }});
+		$scope.$apply();
+	});	
+	$scope.$watch("rebind_domain", function(){
+		if(!$scope.server) return;
+		$scope.dnsmasq.rebind_domain.value = $scope.rebind_domain.map(function(x){ return x.label });
+	}, true);
+	$scope.$watch("server", function(){
+		if(!$scope.server) return;
+		$scope.dnsmasq.server.value = $scope.server.map(function(x){ return x.label });
+	}, true);
+	$scope.$watch("bogusnxdomain", function(){
+		if(!$scope.bogusnxdomain) return;
+		$scope.dnsmasq.bogusnxdomain.value = $scope.bogusnxdomain.map(function(x){ return x.label });
+	}, true);
+	$scope.$watch("hostfiles", function(){
+		if(!$scope.hostfiles) return;
+		$scope.dnsmasq.addnhosts.value = $scope.hostfiles.map(function(x){return x.label});
+	}, true);
+	$scope.on_port_change = function(option){
+		if($scope.dnsmasq[option] && $scope.dnsmasq[option].value){
+			$scope.dnsmasq[option].value = $scope.dnsmasq[option].value.replace(/[^0-9]/g, "");
+		}
+	};
+});
