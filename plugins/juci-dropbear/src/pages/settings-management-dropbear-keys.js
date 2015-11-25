283ca83197d6f35a8339930d84c74dd80fb119c0
Stefan Nygren <no@mail>
2015-11-25 17:41:31 +0100

    Add onKeyAdd command

--- plugins/juci-dropbear/src/pages/settings-management-dropbear-keys.js
+++ plugins/juci-dropbear/src/pages/settings-management-dropbear-keys.js
@@ -1,28 +1,33 @@
 //! Author: Stefan Nygren <stefan.nygren@hiq.se>
 
-JUCI.app.controller("dropbear_key_management", function($scope, $rpc, $uci){
-    $rpc.juci.dropbear.get_public_keys().done(function(keys){
+JUCI.app.controller("dropbear_key_management", function($scope, $rpc, $uci,dropbearAddKey){
+    function refresh(){
+		$rpc.juci.dropbear.get_public_keys().done(function(keys){
 		$scope.keyList = keys;
-        $scope.$apply();
-	
-    });
+		$scope.$apply();
+		});
+    }
 
+	refresh();
 	$scope.getItemTitle = function(item){
         return item.id;
     }
-	
 	$scope.onAddKey = function(){
             $scope.$apply();
         }
-    
 	$scope.onDeleteKey = function(item){
 		   $rpc.juci.dropbear.remove_public_key(item).done(function(res){
 				$scope.stc= res.status;
 				$scope.$apply();
+				refresh();
 			});
         }
-    
-
-
+    $scope.onAddKey = function(){
+        dropbearAddKey.show().done(function(data){
+			$rpc.juci.dropbear.add_public_key(data).done(function(){
+                refresh();
+            });
+        });
+    }
 });
 
