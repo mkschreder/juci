cef0b5e372126f9b962e430800b014ec35ca5397
Martin Schröder <mkschreder.uk@gmail.com>
2015-12-03 14:56:40 +0100

    juci-list-editor: add support for sortable attribute and sorting items.
    - new attribute: sortable (enable in place sorting)
    - new attribute: on-item-moved (called when an item is moved. Parameters: $item, $prev_index, $cur_index)

--- juci/src/widgets/juci-list-editor.js
+++ juci/src/widgets/juci-list-editor.js
@@ -6,11 +6,13 @@ JUCI.app
 			items: "=ngItems", 
 			editor: "@itemEditor", 
 			editable: "@allowEdit", 
+			sortable: "=sortable",
 			getItemTitle: "&getItemTitle", 
 			onCreate: "&onCreate", 
 			onDelete: "&onDelete", 
 			onUpdate: "&onUpdate", 
 			onEditStart: "&onEditStart",
+			onItemMoved: "&onItemMoved",
 			hideButtons: "@hideButtons"
 		}, 
 		controller: "juciListEditor", 
@@ -34,5 +36,24 @@ JUCI.app
 	$scope.onListRemoveItem = function(i){
 		$scope.onDelete({"$item": i});  
 		$scope.item = null; //$scope.items.find(function(x){ return x == i }); 
+	}
+	$scope.onMoveUp = function(i){
+		var arr = $scope.items; 
+		var idx = arr.indexOf(i); 
+		// return if either not found or already at the top
+		if(idx == -1 || idx == 0) return; 
+		arr.splice(idx, 1); 
+		arr.splice(idx - 1, 0, i); 
+		$scope.onItemMoved({ $item: i, $prev_index: idx, $new_index: idx - 1}); 
+	}
+
+	$scope.onMoveDown = function(i){
+		var arr = $scope.items; 
+		var idx = arr.indexOf(i); 
+		// return if either not found or already at the bottom
+		if(idx == -1 || idx == arr.length - 1) return;
+		arr.splice(idx, 1); 
+		arr.splice(idx + 1, 0, i); 
+		$scope.onItemMoved({ $item: i, $prev_index: idx, $new_index: idx + 1}); 
 	}
 }); 
