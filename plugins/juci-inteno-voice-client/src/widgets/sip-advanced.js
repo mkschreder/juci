154cf8bbeb59e9cd78657ade7aa6193f01b3ccc6
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-12-14 12:09:31 +0100

    juci-inteno-voice-client: aded sip-advanced widget

--- plugins/juci-inteno-voice-client/src/widgets/sip-advanced.js
+++ plugins/juci-inteno-voice-client/src/widgets/sip-advanced.js
@@ -8,13 +8,37 @@ JUCI.app
 		replace: true,
 		controller: "sipAdvancedCtrl"
 	};
-}).controller("sipAdvancedCtrl", function($scope, $uci, $tr, gettext){
+}).controller("sipAdvancedCtrl", function($scope, $uci, $tr, gettext, $network){
 	$uci.$sync("voice_client").done(function(){
 		$scope.sip = $uci.voice_client.SIP; 
 		$scope.proxys = $scope.sip.sip_proxy.value.map(function(x){return {label: x}});
+		$scope.localnets = $scope.sip.localnet.value.map(function(x){return {label: x}});
+		$scope.$apply();
+	});
+	$network.getWanNetworks().done(function(data){
+		$scope.wan_networks = data.map(function(x){return { label: String(x[".name"]).toUpperCase(), value: x[".name"] }; });
+		$scope.wan_networks.push({ label: $tr(gettext("Listen on all interfaces")),	value: "" });
 		$scope.$apply();
 	});
 	$scope.onProxyAdded = function(){
 		$scope.sip.sip_proxy.value = $scope.proxys.map(function(x){return x.label});
+	};
+	$scope.check_range = function(value){
+		if($scope.sip.rtpstart.value > $scope.sip.rtpend.value){	
+			$scope.sip.rtpstart.value = $scope.sip.rtpend.value = value;
+		}
+	};
+	$scope.congest_tones = [
+		{ label: $tr(gettext("Congestion")),	value: "congestion" },
+		{ label: $tr(gettext("Info")),			value: "info" }
+	];
+	$scope.dtmf_modes = [
+		{ label: $tr(gettext("Compatibility")),	value: "compatibility" },
+		{ label: $tr(gettext("RFC 2833")),	value: "rfc2833" },
+		{ label: $tr(gettext("SIP INFO")),	value: "info" },
+		{ label: $tr(gettext("Inband")),	value: "inband" },
+	];
+	$scope.onLocalnetAdded = function(){
+		$scope.sip.localnet.value = $scope.localnets.map(function(x){return x.label});
 	};
 });
