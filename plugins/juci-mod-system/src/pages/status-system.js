8b34635a0d88abaaf45fafcfb388b88d07a64a6a
Martin Schröder <mkschreder.uk@gmail.com>
2015-12-17 19:05:04 +0100

    juci-mod-system: only show kernel version and build id in expert mode

--- plugins/juci-mod-system/src/pages/status-system.js
+++ plugins/juci-mod-system/src/pages/status-system.js
@@ -7,7 +7,7 @@ UCI.juci.$registerSectionType("pagesystemstatus", {
 UCI.juci.$insertDefaults("pagesystemstatus"); 
 
 JUCI.app
-.controller("StatusSystemPage", function ($scope, $rootScope, $uci, $rpc, gettext, $tr) {
+.controller("StatusSystemPage", function ($scope, $rootScope, $uci, $rpc, gettext, $tr, $config) {
 	$scope.systemStatusTbl = {
 		rows: [["", ""]]
 	}; 
@@ -64,13 +64,17 @@ JUCI.app
 				[$tr(gettext("Model")), board.model || info.system.hardware || $tr(gettext("N/A"))],
 				[$tr(gettext("Release")), board.release.description || info.system.firmware || $tr(gettext("N/A"))],
 				[$tr(gettext("Firmware Version")), board.release.revision || $tr(gettext("N/A"))],
-				[$tr(gettext("Kernel Version")), board.kernel || info.system.kernel || $tr(gettext("N/A"))],
-				[$tr(gettext("Target")), board.release.target || board.system || info.system.socver || $tr(gettext("N/A"))], 
 				[$tr(gettext("Local Time")), new Date(sys.localtime * 1000)],
 				[$tr(gettext("Uptime")), timeFormat(sys.uptime)],
 				[$tr(gettext("System Load Avg. (1m)")), ""+(info.load.avg[0] / 10.0) + "%"], 
 				[$tr(gettext("CPU")), ""+(cpu_load || 0)+"%"]
 			]; 
+			if($config.mode == "expert"){
+				var arr = $scope.systemStatusTbl.rows; 
+				arr.push([$tr(gettext("Kernel Version")), board.kernel || info.system.kernel || $tr(gettext("N/A"))]); 
+				arr.push([$tr(gettext("Target")), board.release.target || board.system || info.system.socver || $tr(gettext("N/A"))]);  
+			}
+			
 			$scope.systemMemoryTbl.rows = [
 				[$tr(gettext("Usage")), '<juci-progress value="'+Math.round((sys.memory.total - sys.memory.free) / 1000)+'" total="'+ Math.round(sys.memory.total / 1000) +'" units="kB"></juci-progress>'],
 				[$tr(gettext("Shared")), '<juci-progress value="'+Math.round(sys.memory.shared / 1000)+'" total="'+ Math.round(sys.memory.total / 1000) +'" units="kB"></juci-progress>'],
