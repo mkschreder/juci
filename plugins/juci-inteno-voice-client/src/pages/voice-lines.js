29bc542958917ebea44d9d006202ee479f1f50db
Reidar Cederqvist <reidar.cederqvist@gmail.com>
2015-12-18 13:20:04 +0100

    juci-inteno-voice-client: added toggle for different phone-lines pages

---
+++ plugins/juci-inteno-voice-client/src/pages/voice-lines.js
@@ -0,0 +1,6 @@
+//! Author: Reidar Cederqvist <reidar.cederqvist@gmail.com>
+
+JUCI.app.controller("voiceLinesPage", function($scope){
+	$scope.showExpandable = {};
+	$scope.showExpandable.value = true;
+});
