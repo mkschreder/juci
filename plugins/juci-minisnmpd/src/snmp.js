b310a449042218c2e9e358451c43c8a141dae8e8
Martin Schröder <mkschreder.uk@gmail.com>
2015-12-22 11:32:56 +0100

    consistent GPL file headers to comply with the GPL

--- plugins/juci-minisnmpd/src/snmp.js
+++ plugins/juci-minisnmpd/src/snmp.js
@@ -1,5 +1,18 @@
-//! Author: Martin K. Schröder <mkschreder.uk@gmail.com>
-//! Copyright 2015 (c) Martin K. Schröder
+/*	
+	This file is part of JUCI (https://github.com/mkschreder/juci.git)
+
+	Copyright (c) 2015 Martin K. Schröder <mkschreder.uk@gmail.com>
+
+	This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/ 
 
 UCI.$registerConfig("snmpd"); 
 UCI.snmpd.$registerSectionType("mini_snmpd", {
