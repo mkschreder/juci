5f3c6d6cace1acc2d0114728f71be175891fe5b3
Martin Schröder <mkschreder.uk@gmail.com>
2016-05-24 14:49:21 +0200

    juci-firewall-fw3: minor cleanup and translation fixes

--- plugins/juci-firewall-fw3/src/pages/internet-firewall-rules.js
+++ plugins/juci-firewall-fw3/src/pages/internet-firewall-rules.js
@@ -15,7 +15,7 @@
 */ 
 
 JUCI.app
-.controller("InternetFirewallRulesPage", function($scope, $uci, $firewall){
+.controller("InternetFirewallRulesPage", function($scope, $uci, $firewall, $tr, gettext){
 	$firewall.getRules().done(function(rules){
 		$scope.rules = rules; 
 		$scope.$apply(); 
@@ -33,14 +33,14 @@ JUCI.app
 		$uci.firewall.$create({
 			".type": "rule", 
 			"name": "new_rule"
-		}).done(function(rule){
+		}).done(function(){
 			$scope.$apply(); 
 		}); 
 	}
 	
 	$scope.onDeleteRule = function(rule){
-		if(!rule) alert(gettext("Please select a rule to delete!")); 
-		if(confirm(gettext("Are you sure you want to delete this rule?"))){
+		if(!rule) alert($tr(gettext("Please select a rule to delete!")));
+		if(confirm($tr(gettext("Are you sure you want to delete this rule?")))){
 			rule.$delete().done(function(){
 				$scope.$apply(); 
 			}); 
